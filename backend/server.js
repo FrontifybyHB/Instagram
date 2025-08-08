a6b4b417333240bb807ba53e64f159961d3e0ac4
Himanshu <work.himanshu.hb@gmail.com>
2025-08-08 13:53:25 +0530

    socket io

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,25 @@
-import app from "./src/app.js"; 
+import app from "./src/app.js";
 import connectDB from './src/db/db.js'
 import config from './src/config/config.js'
+import { createServer } from "http";
+import { Server } from "socket.io";
 
 const port = config.PORT;
+const httpServer = createServer(app);
+
+const io = new Server(httpServer, { /* options */ });
+
+io.on("connection", (socket) => {
+  console.log('A user connected')
+
+  socket.on("disconnect", () => {
+    console.log('A user disconnect')
+  })
+});
+
 
 connectDB();
 
-app.listen(port, () => {
+httpServer.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
